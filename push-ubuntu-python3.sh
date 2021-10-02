77606c310816949720738fef357d776d64be349a
Martin Leischner <martin.leischner@h-brs.de>
2021-10-02 09:30:55 +0200

    version v1.0.2

--- push-ubuntu-python3.sh
+++ push-ubuntu-python3.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # Script to build a test image (before pushing to git)
 
-TAG=v1.0.1      # Stable Version 27.07.21
+TAG=v1.0.2      # Stable Version 02.10.2021
 NAME=ubuntu-python3
 
 # Tag the image
 docker tag jennerwein/${NAME}:test jennerwein/${NAME}:${TAG}
 docker push jennerwein/${NAME}:${TAG}
 
+docker tag jennerwein/${NAME}:test jennerwein/${NAME}:latest
+docker push jennerwein/${NAME}:latest
